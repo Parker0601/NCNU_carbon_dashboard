6b1a057f44ea7255d1fa94b316ff4d3bd79202ea
翔\Acer6 <chewqixiang09062003@gmail.com>
2025-08-24 00:47:30 +0800

    修復了npm run dev時無法連上frontend的問題

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,5 +1,33 @@
 'use strict';
 
-var requireDir = require('require-dir');
+const gulp = require('gulp');
+const { series } = require('gulp');
+const requireDir = require('require-dir');
 
-requireDir('./build', {recurse: true});
+// 1) 載入 build/ 目錄下所有任務檔（如 build/serve.js、build/dev.js 等）
+requireDir('./build', { recurse: true });
+
+// 2) 自動挑選 default 任務（依序嘗試這些名稱）
+const candidates = ['serve', 'dev', 'watch', 'start', 'build'];
+const hasTask = (name) => {
+  try {
+    return !!gulp.registry().get(name);
+  } catch {
+    return false;
+  }
+};
+const chosen = candidates.find(hasTask);
+
+// 3) 設定 default；若沒有任何候選任務，顯示提示訊息
+if (chosen) {
+  gulp.task('default', series(chosen));
+  console.log(`[gulp] Default task set to "${chosen}"`);
+} else {
+  gulp.task('default', function noDefault(cb) {
+    console.log('[gulp] No serve/dev/watch/start/build task found under ./build');
+    console.log('[gulp] Please create one (e.g. build/serve.js registers a "serve" task).');
+    cb();
+  });
+}
+
+module.exports = gulp;

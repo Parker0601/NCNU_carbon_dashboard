30296bc7e925bda79a2bf55430e1693c22433fba
翔\Acer6 <chewqixiang09062003@gmail.com>
2025-08-25 23:25:12 +0800

    Revert "修復了npm run dev時無法連上frontend的問題"
    This reverts commit 6b1a057f44ea7255d1fa94b316ff4d3bd79202ea.

--- frontend/build/connect.js
+++ frontend/build/connect.js
@@ -5,7 +5,7 @@ gulp.task('connect', function (done) {
 	connect.server({
 		root: 'dist/',
 		livereload: false,
-		port: 8080,
+		port: 4000,
 		fallback: 'dist/page_login.html'   ///這裡控制開啟server後首先會看到的網頁
 	});
 	done();
